3121da4693850263747b20353fcceab6046bf1ea
arucoder <78599044+arucoder@users.noreply.github.com>
2021-02-05 18:03:53 +0530

    Add files via upload

---
+++ javascript crash course/home.js
@@ -0,0 +1,89 @@
+// console.log('hello');
+
+// // alert('hey alert');
+
+// // variables
+// var b = 'smoothies';
+// console.log(b);
+
+// var someNumber = 45;
+// console.log(someNumber);
+
+// var age = prompt('what is your age ?');
+
+// document.getElementById('Aravind').innerHTML = age;
+
+// // Number 
+// var num1 = 10;
+// num1 = num1 + 2;
+// console.log(num1);
+
+// // Function
+// function fun() {
+//     alert('this is a function'); 
+// }
+
+// fun();
+
+// function greeting() {
+//     var name = prompt('what is your name ?');
+//     var result = 'hello' +' ' + name ;
+//     console.log(result);
+// }
+
+// greeting();
+
+var num = 0;
+
+while(num < 100) {
+    num += 1;
+    console.log(num);
+}
+
+let yourAge = 18
+let yourName = 'aru'
+let name = {first: 'Jane', last: 'Doe'};
+let truth = false;
+let groceries = ['apple'];
+let nothing = null;
+
+let fruitName = 'Orange';
+console.log(fruitName);
+
+console.log(fruitName.length);
+
+console.log(fruitName.indexOf('an'));
+
+console.log(fruitName.replace('ban', '123'));
+
+console.log(fruitName.toUpperCase(fruitName));
+
+console.log(fruitName.toLowerCase(fruitName));
+
+// Array
+
+let fruit = ['banana']
+
+let fruits = new Array('apple')
+
+// object
+
+let student = {first: 'rafeh' ,  last: 'qazi'}
+console.log(student.last);
+
+// Conditions 
+// && ---- > AND
+// || ----- > OR
+var age = prompt('what is your age?');
+
+if ((age >= 18) && (age <= 35) ) {
+    status = 'target demo';
+    console.log(status);
+} else {
+    status = 'not my audience';
+    console.log(status);
+}
+
+// JSON
+
+
